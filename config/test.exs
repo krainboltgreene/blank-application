65801c8ee93c2f29f815819f1c873216414052b0
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-05-02 18:29:56 -0700

    Removing oban in favor of faktory

--- config/test.exs
+++ config/test.exs
@@ -8,12 +8,6 @@ config :henosis, Henosis.Database.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
-# Oban configuration
-config :henosis, Oban,
-  crontab: false,
-  queues: false,
-  prune: :disabled
-
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :henosis, HenosisWeb.Endpoint,
