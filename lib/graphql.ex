a47811423a2b2dcc37d1692edeee1726dbbc5b3d
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-08-26 01:10:30 -0700

    Don't need

--- lib/graphql.ex
+++ lib/graphql.ex
@@ -1,18 +1,3 @@
 defmodule Graphql do
-  @moduledoc """
-  Documentation for `Graphql`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Graphql.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
