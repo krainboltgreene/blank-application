44e8bd32aae38aad0ccccfc62d95aace0724a96d
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-09-02 08:04:58 -0700

    Creating some new atoms

--- lib/browser/atoms/index.ts
+++ lib/browser/atoms/index.ts
@@ -1,2 +1,4 @@
-export {default as isSessionPresent} from "./isSessionPresent";
 export {default as currentAccount} from "./currentAccount";
+export {default as localDatabase} from "./localDatabase";
+export {default as cookie} from "./cookie";
+export {default as warningMessages} from "./warningMessages";
