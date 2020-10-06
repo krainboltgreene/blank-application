6c3893b587b6e411a86f926574e85f65b159c2f6
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-10-06 11:38:31 -0700

    Lost this

---
+++ priv/repository/migrations/20200127021839_create_organization_permissions.exs
@@ -0,0 +1,22 @@
+defmodule Database.Repository.Migrations.CreateOrganizationAccountPermissions do
+  use Ecto.Migration
+
+  def change do
+    create table(:organization_permissions, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+
+      add :permission_id,
+          references(:permissions, on_delete: :nothing, type: :binary_id),
+          null: false
+
+      add :organization_membership_id,
+          references(:organization_memberships, on_delete: :nothing, type: :binary_id),
+          null: false
+
+      timestamps()
+    end
+
+    create unique_index(:organization_permissions, [:permission_id, :organization_membership_id])
+    create index(:organization_permissions, [:organization_membership_id])
+  end
+end
