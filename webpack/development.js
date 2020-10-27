b07ac013a1667ea95e461f13a8a414c8eeb597fb
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-10-27 16:57:37 -0700

    Webpack 5 readiness

--- webpack/development.js
+++ webpack/development.js
@@ -37,7 +37,7 @@ module.exports = {
         use: {
           loader: "file-loader",
           options: {
-            name: "[name].[hash].[ext]",
+            name: "[name].[contenthash].[ext]",
           },
         },
       },
