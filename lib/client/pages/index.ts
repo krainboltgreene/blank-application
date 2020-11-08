b99c12868056643a63fe8baa6adfd39a934b5ecd
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-07 19:38:53 -0800

    Sorting

--- lib/client/pages/index.ts
+++ lib/client/pages/index.ts
@@ -1,13 +1,13 @@
+export {default as AccountConfirmation} from "./AccountConfirmation";
 export {default as CodeOfConduct} from "./CodeOfConduct";
 export {default as DataPolicy} from "./DataPolicy";
 export {default as LandingPage} from "./LandingPage";
+export {default as Login} from "./Login";
 export {default as Logout} from "./Logout";
 export {default as OurTechnology} from "./OurTechnology";
 export {default as PageNotFound} from "./PageNotFound";
 export {default as PrivacyPolicy} from "./PrivacyPolicy";
 export {default as Settings} from "./Settings";
+export {default as SignUp} from "./SignUp";
 export {default as TermsOfService} from "./TermsOfService";
 export {default as ThisIsUs} from "./ThisIsUs";
-export {default as SignUp} from "./SignUp";
-export {default as Login} from "./Login";
-export {default as AccountConfirmation} from "./AccountConfirmation";
