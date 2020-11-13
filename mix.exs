1aaff5f47ed618da7f75fd8165b6d4602ff4bf54
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-12 17:29:19 -0800

    Auto generate schema on compile

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule ClumsyChinchilla.MixProject do
       version: "1.0.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers() ++ [:graphql_schema_json, :graphql_schema_sdl],
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
