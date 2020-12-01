3f7664fa2f28eee1c421a270870310199b15d359
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-30 22:57:43 -0800

    No silly no-null rule

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = {
     ["@babel/preset-env", {useBuiltIns: "entry", corejs: 3, targets: "> 0.25%, not dead"}],
     ["@babel/preset-typescript", {isTSX: true, allExtensions: true}],
     ["@babel/preset-react", {development: process.env.NODE_ENV !== "production"}],
-    process.env.NODE_ENV === "production" ? "minify" : undefined,
+    process.env.NODE_ENV === "production" ? "minify" : null,
     "@emotion/babel-preset-css-prop",
   ].filter((preset) => preset),
   plugins: [
@@ -13,7 +13,7 @@ module.exports = {
       "^@clumsy_chinchilla/(.+)$": "./lib/client/\\1",
       "^@assets/(.+)$": "./assets/\\1",
     }}],
-    process.env.NODE_ENV === "production" ? undefined : "react-hot-loader/babel",
-    process.env.NODE_ENV === "production" ? undefined : "annotate-console-log",
+    process.env.NODE_ENV === "production" ? null : "react-hot-loader/babel",
+    process.env.NODE_ENV === "production" ? null : "annotate-console-log",
   ].filter((preset) => preset),
 };
