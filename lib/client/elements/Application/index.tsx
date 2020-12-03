af1c60767fee773bdfcea0937cb39cce1f5e5b69
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-12-03 08:52:03 -0800

    Easy way to do a CSS reboot

--- lib/client/elements/Application/index.tsx
+++ lib/client/elements/Application/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import {Global} from "@emotion/react";
+import {reboot} from "@clumsy_chinchilla/styles";
 import ErrorBoundary from "./ErrorBoundary";
 import MaybeAuthenticated from "./MaybeAuthenticated";
 import WithCookies from "./WithCookies";
@@ -7,6 +9,7 @@ import Router from "./Router";
 
 export default function Application (): JSX.Element {
   return <ErrorBoundary>
+    <Global styles={reboot} />
     <MaybeAuthenticated>
       <WithCookies>
         <WithLocalDatabase>
