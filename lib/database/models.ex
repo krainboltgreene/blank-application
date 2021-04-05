578517b560b9f0309e25929cf15e977aa1a8aaa2
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2021-04-04 19:26:42 -0700

    Moving common behavior into a shared location

---
+++ lib/database/models.ex
@@ -0,0 +1,22 @@
+defmodule Database.Models do
+  @spec has_standard_behavior :: {:__block__, [], [{:def, [...], [...]}, ...]}
+  defmacro has_standard_behavior() do
+    quote do
+      def create(attributes) do
+        changeset(%__MODULE__{}, attributes)
+        |> case do
+          %Ecto.Changeset{valid?: true} = changeset -> Database.Repository.insert(changeset)
+          %Ecto.Changeset{valid?: false} = changeset -> {:error, changeset}
+        end
+      end
+
+      def update(record, attributes) do
+        changeset(record, attributes)
+        |> case do
+          %Ecto.Changeset{valid?: true} = changeset -> Database.Repository.update(changeset)
+          %Ecto.Changeset{valid?: false} = changeset -> {:error, changeset}
+        end
+      end
+    end
+  end
+end
