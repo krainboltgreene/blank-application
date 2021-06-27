f9a8fb9e5e355bc0fb91f2af0b8192261ce02d3d
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2021-06-27 02:28:51 +0000

    More terraform stuff

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -3,7 +3,8 @@
 . $HOME/.asdf/asdf.sh &&
 echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc &&
 echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc &&
-asdf global terraform 1.0.0 &&
+asdf global terraform 1.0.1 &&
+terraform init &&
 terraform apply &&
 mix do local.hex --force, local.rebar --force, deps.get, compile, dialyzer.build, ecto.setup &&
 mix do compile, dialyzer.build, ecto.setup &&
