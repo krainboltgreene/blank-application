635e49987a597801540ee5ff713f204db66354fb
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2021-06-27 03:58:50 +0000

    Making sure the ssh key is loaded

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -6,8 +6,10 @@ echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc &&
 asdf shell elixir 1.12.1-otp-24 &&
 asdf shell nodejs 16.4.0 &&
 asdf shell terraform 1.0.1 &&
-terraform init &&
-terraform apply &&
 mix do local.hex --force, local.rebar --force, deps.get, compile, dialyzer.build, ecto.setup &&
 mix do compile, dialyzer.build, ecto.setup &&
-npm install --no-audit
+npm install --no-audit &&
+terraform init &&
+terraform apply &&
+eval "$(ssh-agent -s)" &&
+ssh-add ~/.ssh/codespaces
