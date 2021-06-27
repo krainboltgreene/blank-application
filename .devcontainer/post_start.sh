7ecf44d439d01ce081e94a62e1248f22123bd5f6
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2021-06-27 00:55:31 +0000

    Make sure to be using the right terraform

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -3,6 +3,7 @@
 . $HOME/.asdf/asdf.sh &&
 echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc &&
 echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc &&
+asdf global terraform 1.0.0 &&
 terraform apply &&
 mix do local.hex --force, local.rebar --force, deps.get, compile, dialyzer.build, ecto.setup &&
 mix do compile, dialyzer.build, ecto.setup &&
