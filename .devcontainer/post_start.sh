182829df6383b49bc495eb4a2cd2d2122aa6a8c4
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2021-08-15 21:07:47 +0000

    Fixing some things

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -2,7 +2,8 @@
 
 . $HOME/.asdf/asdf.sh &&
 mix do deps.get, compile, dialyzer.build, ecto.reset &&
-npm install --no-audit
+npm install --no-audit &&
+echo $CODESPACE_PACKAGE_TOKEN  | docker login ghcr.io -u krainboltgreene  --password-stdin
 # terraform init &&
 # terraform apply &&
 # eval "$(ssh-agent -s)" &&
