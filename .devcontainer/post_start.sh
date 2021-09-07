609a285b50bdfa638f4b2e8bdba1ab4977aba3c3
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2021-09-07 08:24:02 +0000

    WIP

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env zsh
 
 . $HOME/.asdf/asdf.sh &&
-mix do deps.get, compile, dialyzer.build, ecto.reset &&
+mix do deps.get, compile &&
 npm install --no-audit &&
+mix dialyzer.build &
+mix ecto.reset &
 echo $CODESPACE_PACKAGE_TOKEN | docker login ghcr.io -u krainboltgreene --password-stdin
 # terraform init &&
 # terraform apply &&
