a31d4c562672058c4907e94efeea27b3b6f4a41e
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2021-11-14 10:44:42 +0000

    Cleaning up the storybook work

--- lib/client/elements/Card/story.tsx
+++ lib/client/elements/Card/story.tsx
@@ -1,19 +1,18 @@
-/* eslint-disable func-style */
-import {number} from "@storybook/addon-knobs";
-import {text} from "@storybook/addon-knobs";
+import React, { ComponentProps } from 'react';
+import { Story, Meta } from '@storybook/react';
 import Card from ".";
 
+//👇 This default export determines where your story goes in the story list
 export default {
+  title: 'elements/Card',
   component: Card,
-  title: "elements/Card",
-};
+} as Meta;
 
-function Template (properties) {
-  return <Card {...properties} />;
-}
-export const Default = Template.bind({});
+//👇 We create a “template” of how args map to rendering
+const Template: Story<ComponentProps<typeof Card>> = (args) => <Card {...args} />;
 
-Default.args = {
+export const Sample = Template.bind({});
+Sample.args = {
   name: "The Ambassador",
   description: "When I finish a project, swap a project.",
   cost: 12,
@@ -22,18 +21,3 @@ Default.args = {
   planning: 6,
   guile: 4,
 };
-
-// export function sample (): JSX.Element {
-//   return <BrowserRouter>
-//     <Card
-
-// name text("name", "The Ambassador"),
-//       description={text("description", "When I finish a project, swap a project.")}
-//       cost={number("cost", 12)}
-//       leadership={number("leadership", 6)}
-//       wealth={number("wealth", 4)}
-//       planning={number("planning", 6)}
-//       guile={number("guile", 4)}
-//     />
-//   </BrowserRouter>;
-// }
