6f0935f1d0533588246845e17714609c848f3e2c
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2022-02-18 23:18:37 +0000

    Cleaning up some of the devcontainer stuff

--- .devcontainer/library-scripts/erlang-debian.sh
+++ .devcontainer/library-scripts/erlang-debian.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-curl -fSL -o erlang.deb https://packages.erlang-solutions.com/erlang/debian/pool/esl-erlang_24.2.1-1~ubuntu~focal_amd64.deb &&
+DEBIAN_FRONTEND=noninteractive &&
+	apt-get install --no-install-recommends -y libncurses5 libwxgtk3.0-gtk3-dev libsctp1 libssh-dev xsltproc fop libxml2-utils &&
+	curl -fSL -o erlang.deb https://packages.erlang-solutions.com/erlang/debian/pool/esl-erlang_24.2.1-1~ubuntu~focal_amd64.deb &&
 	dpkg -i erlang.deb &&
 	rm erlang.deb
