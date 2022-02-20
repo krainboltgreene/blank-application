c6a4c8b38d23af65a36d020ed1c473f642f485aa
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2022-02-20 05:28:38 +0000

    Forgot to add a filechange watcher

--- .devcontainer/library-scripts/elixir-debian.sh
+++ .devcontainer/library-scripts/elixir-debian.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-curl -fSL -o elixir.deb https://packages.erlang-solutions.com/erlang/debian/pool/elixir_1.13.0-1~ubuntu~focal_all.deb &&
+DEBIAN_FRONTEND=noninteractive &&
+	apt-get install --no-install-recommends -y inotify-tools &&
+	curl -fSL -o elixir.deb https://packages.erlang-solutions.com/erlang/debian/pool/elixir_1.13.0-1~ubuntu~focal_all.deb &&
 	dpkg -i elixir.deb &&
 	rm elixir.deb &&
 	mix do local.hex --force, local.rebar --force
